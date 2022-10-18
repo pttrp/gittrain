8538c94c58a09590118b2cd3c745ae1cb0986928
Pavel Taratinov <p.taratinov@vk.team>
2022-10-18 10:36:28 +0300

    2nd change

--- firstfile.py
+++ firstfile.py
@@ -1,14 +1,3 @@
 #!/usr/bin/env python3
 print("hello, world!")
-print("another Test!")
-print("adding hotfix2")
-<<<<<<< HEAD
-<<<<<<< HEAD
-print("3rd commit")
-=======
-print("4th commit")
->>>>>>> ADM-1
-=======
-print("4th commit")
->>>>>>> hotfix2
-
+print(2nd change")
